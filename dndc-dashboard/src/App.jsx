591e7da82101abf788a3e410722ad8318b0750e3
tanish114 <97626851+tanish114@users.noreply.github.com>
2026-05-22 15:25:28 +0530

    new3

--- dndc-dashboard/src/App.jsx
+++ dndc-dashboard/src/App.jsx
@@ -4,7 +4,7 @@ import {
   Route
 } from "react-router-dom";
 
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
